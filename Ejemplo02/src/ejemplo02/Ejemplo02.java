26ea25c65d1fcc130dfa36ede5f8bd2187a6846e
@reroes <reroes799@gmail.com>
2020-11-04 02:53:02 -0500

    ejemplos clase 04

---
+++ Ejemplo02/src/ejemplo02/Ejemplo02.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplo02;
+
+import java.util.Scanner;
+
+public class Ejemplo02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Ingreso de datos por teclado
+        // uso de la clase Scanner
+        Scanner entrada = new Scanner(System.in);
+    }
+    
+}
